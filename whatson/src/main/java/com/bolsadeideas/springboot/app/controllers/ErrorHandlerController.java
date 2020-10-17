8d8b758e8daf5aaf4be2914c63beabd4ce3cfc6c
Gonzalo <gcascon@gmail.com>
2020-10-17 15:51:57 +0200

    'errores'

---
+++ whatson/src/main/java/com/bolsadeideas/springboot/app/controllers/ErrorHandlerController.java
@@ -0,0 +1,22 @@
+package com.bolsadeideas.springboot.app.controllers;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ErrorHandlerController {
+
+	@ExceptionHandler(Exception.class)
+	public String aritmeticaError(Exception ex, Model model) {
+		model.addAttribute("error", "Ups! Se ha producido un en la aplicación :-(");
+		model.addAttribute("message", ex.getMessage());
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		model.addAttribute("timestamp", new Date());
+		return "error/generica";
+	}
+	
+}
